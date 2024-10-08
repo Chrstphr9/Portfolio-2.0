a8f2276fa16a4cf8a40f1921f0491d35e10c0d5d
somto <somto565@gmail.com>
2024-10-08 16:09:29 +0100

    Updated Home component: removed flex-1 class, added new Portfolio section with heading, paragraphs, and spans.

--- app/page.jsx
+++ app/page.jsx
@@ -61,7 +61,7 @@ export default function Home() {
             </p>
           </div>
           <div>
-          <div className="text-center shadow-lg p-10 rounded-xl my-10 border border-sectxt hover:shadow-[-7px_7px_0px_#FFD739] flex-1">
+          <div className="text-center shadow-lg p-10 rounded-xl my-10 border border-sectxt hover:shadow-[-7px_7px_0px_#FFD739]">
               <Image src={design} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
@@ -91,6 +91,23 @@ export default function Home() {
               <p className="text-pribg py-1">Postman</p>
               <p className="text-pribg py-1">Firebase</p>
             </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-pribg">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-secbg"> agencies </span>
+              consulted for <span className="text-secbg">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-secbg ">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
           </div>
         </section>
       </main>
