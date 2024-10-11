504c46a7ebf6da77e68b1fcc10e8267722aba00f
somto <somto565@gmail.com>
2024-10-11 18:01:46 +0100

    Updated page.jsx with various styling and layout changes, added font icons and external link functionality to portfolio section

--- app/page.jsx
+++ app/page.jsx
@@ -20,6 +20,7 @@ import work8 from '../public/work8.png'
 import work9 from '../public/work9.png'
 import { useState } from "react";
 import resume from '../public/Resume.pdf'
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 
 export default function Home() {
@@ -56,8 +57,8 @@ export default function Home() {
 
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-pribg font-medium md:text-6xl'>Anigbo Somto</h2>
-            <h3 className='text-2xl py-2 md:text-3xl text-pribg'>Not Your Regular Developer</h3>
-            <p className='text-md py-5 leading-8 text-pribg md:text-xl max-w-xl mx-auto'>Freelancer providing services for programming and design content
+            <h3 className='text-2xl py-2 md:text-3xl text-pribg font-semibold'>Not Your Regular Developer</h3>
+            <p className='text-xl font-semibold py-5 leading-8 text-pribg md:text-xl max-w-xl mx-auto'>Freelancer providing services for programming and design content
             needs. Join me down below and let's get cracking!</p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-pribg'>
@@ -74,7 +75,7 @@ export default function Home() {
         <section>
           <div>
             <h3 className='text-3xl py-1 font-semibold'>Services I Offer</h3>
-            <p className='text-md py-2 leading-8 text-pribg font-semibold'>
+            <p className='text-2xl py-2 leading-8 text-pribg font-semibold'>
             Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
               <span className="text-secbg"> agencies </span>
@@ -82,49 +83,55 @@ export default function Home() {
               and collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-pribg">
+            <p className="text-pribg text-2xl font-semibold py-2 leading-8">
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
           </div>
 
           <div className='lg:flex gap-10 justify-center'>  
-          <div className=" text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white border border-sectxt hover:shadow-[-7px_7px_0px_#FFD739]">
+          <div className="text-xl font-semibold text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:bg-secbg hover:border-2 hover:border-sectxt hover:text-pritxt">
+              <div className='flex justify-center'>
               <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
+              </div>
+              <h3 className="font-bold text-xl pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
+              <div>
               <p className="py-2">
                 Creating elegant designs suited for your needs following core
                 design theory.
               </p>
-              <h4 className="py-4 text-pribg">Design Tools I Use</h4>
-              <p className="text-pribg py-1">React</p>
-              <p className="text-pribg py-1">Tailwind CSS</p>
-              <p className="text-pribg py-1">Figma</p>
-              <p className="text-pribg py-1">Indesign</p>
+              <h4 className="py-4">Design Tools I Use</h4>
+              <p className="py-1">React</p>
+              <p className="py-1">Tailwind CSS</p>
+              <p className="py-1">Figma</p>
+              <p className="py-1">Indesign</p>
+              </div>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white border border-sectxt hover:shadow-[-7px_7px_0px_#FFD739] ">
+            <div className="text-xl font-semibold text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:bg-secbg hover:border-2 hover:border-sectxt hover:text-pritxt">
+              <div className='flex justify-center'>
               <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
+              </div>
+              <h3 className="text-lg font-bold pt-8 pb-2 ">
                 Code your dream project
               </h3>
               <p className="py-2">
                 Do you have an idea for your next great website? Let's make it a
                 reality.
               </p>
-              <h4 className="py-4 text-pribg">Coding Tools I Use</h4>
-              <p className="text-pribg py-1">Vscode</p>
-              <p className="text-pribg py-1">Next.JS</p>
-              <p className="text-pribg py-1">Postman</p>
-              <p className="text-pribg py-1">Firebase</p>
+              <h4 className="py-4" >Coding Tools I Use</h4>
+              <p className="py-1">Vscode</p>
+              <p className="py-1">Next.JS</p>
+              <p className="py-1">Postman</p>
+              <p className="py-1">Firebase</p>
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className='text-3xl py-1'>Portfolio</h3>
-            <p className="text-md py-2 leading-8 text-pribg">
+            <h3 className='text-3xl font-semibold py-1'>Portfolio</h3>
+            <p className="text-2xl font-semibold py-2 leading-8 text-pribg">
               Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
               <span className="text-secbg"> agencies </span>
@@ -132,36 +139,68 @@ export default function Home() {
               and collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-secbg ">
+            <p className="text-2xl font-semibold py-2 leading-8 text-secbg ">
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work1} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work2} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work3} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work4} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work5} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
+               <Image src={work6} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
+            </div>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work7} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work8} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
-            <div className='basis-1/3 flex-1'>
+            <div className='basis-1/3 flex-1  <div className=" text-center p-10 rounded-xl my-10 dark:bg-white border border-sectxt shadow-[-7px_7px_0px_#FFD739] transform transition duration-300 hover:scale-110 hover:border-2 hover:border-sectxt hover:text-pritxt">'>
                <Image src={work9} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+               <a href="https://lovely-rugelach-eb2a44.netlify.app/" className="flex justify-center items-center mt-2 hover:text-pribg hover:scale-110 transition duration-300 ease-in-out">
+              <i className="text-2xl flex font-semibold font-burtons"><FaExternalLinkAlt />View Project</i>
+            </a>
             </div>
+            
+            
           </div>
         </section>
       </main>
